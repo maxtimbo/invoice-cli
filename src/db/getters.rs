f2ad65890022de1bcc75f8c68b67d7ad61fbe4be
Tim Finley <tfinley@pop-os.localdomain>
2024-04-30 22:31:02 -0400

    Added Invoices to list table

--- src/db/getters.rs
+++ src/db/getters.rs
@@ -138,7 +138,10 @@ impl InvoiceDB {
     }
 
     pub fn get_table(&self, table_name: &str) -> Result<Vec<ShortList>, rusqlite::Error> {
-        let query = format!("SELECT id, name FROM {}", table_name);
+        let query = match table_name {
+            "invoices" => format!("SELECT id, date FROM {}", table_name),
+            _ => format!("SELECT id, name FROM {}", table_name)
+        };
 
         let mut stmt = self.connection.prepare(&query)?;
         let short_list_iter = stmt.query_map([], |row| {

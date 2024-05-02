6425d3e13cb6e80b7c79a99b9b74f6a6b7445ee4
Tim Finley <tfinley@pop-os.localdomain>
2024-05-01 21:37:03 -0400

    Added the ability to use images for the logo in the template

--- src/models/company.rs
+++ src/models/company.rs
@@ -1,8 +1,10 @@
 use std::fmt;
-use serde::{Serialize, Deserialize};
+use base64::{engine::general_purpose::STANDARD, Engine};
+use serde::{Serialize, Serializer, ser::SerializeStruct, Deserialize};
 use crate::models::contact::Contact;
+use infer;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Company {
     pub id: i64,
     pub name: String,
@@ -17,4 +19,29 @@ impl fmt::Display for Company {
         write!(f, "Has Logo:\t{}\n", self.logo.is_some())?;
         write!(f, "Contact Information:\n{}", self.contact)
     }
+}
+
+impl Serialize for Company {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let mut state = serializer.serialize_struct("Company", 4)?;
+            state.serialize_field("id", &self.id)?;
+            state.serialize_field("name", &self.name)?;
+
+            if let Some(ref logo_data) = self.logo {
+                if let Some(kind) = infer::get(logo_data) {
+                    let mime_type = kind.mime_type();
+                    let logo_data_uri = format!("data:{};base64,{}", mime_type, STANDARD.encode(logo_data));
+                    state.serialize_field("logo", &logo_data_uri)?;
+                } else {
+                    state.serialize_field("logo", &None::<String>)?;
+                }
+            } else {
+                state.serialize_field("logo", &None::<String>)?;
+            }
+            state.serialize_field("contact", &self.contact)?;
+            state.end()
+        }
 }
